25511df28641476c18c11bfb450dc15613c66454
blueplanet <erguolinge@gmail.com>
2017-03-09 20:23:11 +0900

    3.2

---
+++ ch03/3.py
@@ -0,0 +1,62 @@
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+print("Class labels:", np.unique(y))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0, shuffle=True)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))  # 各特徴量を1次元配列に変換して予測を実行
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)  # 予測結果を元のグリッドポイントのデータサイズに変換
+    Z = Z.reshape(xx1.shape)
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)  # 軸の範囲の設定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # クラスごとにサンプルをプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+
+    # テストサンプルを目立たせる(点を○で表示)
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidths=1, marker='o', s=55, label='test set')
+
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X = X_combined_std, y = y_combined, classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
